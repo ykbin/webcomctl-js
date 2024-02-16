ee32b43a67ec8517193a1f3433da4b2c2f28dd66
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-02-16 21:32:08 +0000

    Added UIStartupUploadControl

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,4 @@
 export { default as UILoggerControl } from './UILoggerControl/index.mjs';
 export { default as UILoadingControl } from './UILoadingControl/index.mjs';
+export { default as UIStartupUploadControl } from './UIStartupUploadControl/index.mjs';
+export { default as UIMainFooterControl } from './UIMainFooterControl/index.mjs';
