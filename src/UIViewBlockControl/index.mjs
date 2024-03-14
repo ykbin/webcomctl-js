d6da91ca74c0c34a7178cfd793f231f5fc4d3e09
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-03-14 19:47:42 +0000

    Added_UIViewBlockControl

---
+++ src/UIViewBlockControl/index.mjs
@@ -0,0 +1,24 @@
+import { BaseControl } from 'webnetq-js';
+
+const CLASS = {
+  ROOT: "uic-viewblk-root",
+  PORT: "uic-viewblk-port",
+};
+
+const _rootHTML = `
+<div class="${CLASS.ROOT} ${CLASS.PORT} notranslate" translate="no">
+</div>
+`;
+
+export default class UIViewBlockControl extends BaseControl {
+  static get template() { return {
+    name: 'ViewBlock',
+    rootHTML: _rootHTML,
+    rootClass:  CLASS.ROOT,
+    portClass:  CLASS.PORT,
+  } }
+
+  constructor(element) {
+    super(element);
+  }
+};
