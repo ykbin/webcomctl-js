0413179dda789719856a2bf263764782c3dd7f6e
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-03-14 22:24:24 +0000

    Added_UIViewBlockControl

--- src/index.mjs
+++ src/index.mjs
@@ -3,4 +3,4 @@ export { default as UILoadingControl } from './UILoadingControl/index.mjs';
 export { default as UIStartupUploadControl } from './UIStartupUploadControl/index.mjs';
 export { default as UIMainFooterControl } from './UIMainFooterControl/index.mjs';
 export { default as UIVerticalBlockControl } from './UIVerticalBlockControl/index.mjs';
-export { default as UIViewBlockControl } from './UIVerticalBlockControl/index.mjs';
+export { default as UIViewBlockControl } from './UIViewBlockControl/index.mjs';
