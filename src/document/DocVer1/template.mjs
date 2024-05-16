69724d9e902ae7ca89521c2d0da4be42c99309a7
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-05-16 13:45:53 +0100

    UIDocVer1Document

---
+++ src/document/DocVer1/template.mjs
@@ -0,0 +1,28 @@
+export const NAME = 'DocVer1';
+
+export const CLASS = {
+  ROOT: "uic-docv1-root",
+  PORT: "uic-docv1-root",
+};
+
+export const HTML = `
+<!DOCTYPE html>
+<html data-theme="light">
+  <head></head>
+  <body class="${CLASS.PORT}"></body>
+</html>
+`;
+
+export const CSS = `
+.body
+{
+  position: fixed;
+  cursor: default;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0;
+  padding: 0;
+}
+`;
