11c4e89dfb5e7cbc0599432a712aa2c3be03d8df
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-05-16 13:45:53 +0100

    UIBodyControl

---
+++ src/UIBodyControl/index.mjs
@@ -0,0 +1,19 @@
+import { BaseControl } from 'webnetq-js';
+import { NAME, HTML, CLASS, CSS } from './template.mjs';
+
+export const template = {
+  NAME, HTML, CLASS, CSS,
+};
+
+export default class UIBodyControl  extends BaseControl {
+  static get template() { return {
+    name: NAME,
+    rootHTML: HTML,
+    rootClass:  CLASS.ROOT,
+    portClass:  CLASS.PORT,
+  } }
+
+  constructor(element) {
+    super(element);
+  }
+};
