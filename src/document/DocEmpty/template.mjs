0a14bcc5c34c7d54a6487515010eabc6f47c8fd3
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-05-27 16:28:24 +0100

    Document Empty Template

---
+++ src/document/DocEmpty/template.mjs
@@ -0,0 +1,17 @@
+export const NAME = 'DocVer1';
+
+export const CLASS = {
+  ROOT: "uic-docemt-root",
+  PORT: "uic-docemt-root",
+};
+
+export const HTML = `
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body class="${CLASS.ROOT}"></body>
+</html>
+`;
+
+export const CSS = `
+`;
