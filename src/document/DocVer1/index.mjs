c8d7329425900107fb60bc8e5a458b98dd3b523c
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-06-29 15:34:32 +0100

    Added PORT_CLASS to documents

--- src/document/DocVer1/index.mjs
+++ src/document/DocVer1/index.mjs
@@ -1,6 +1,6 @@
-import { NAME, ROOT_HTML, ROOT_CLASS, CSS } from 'module-loader!./template.mjs';
+import { NAME, ROOT_HTML, ROOT_CLASS, PORT_CLASS, CSS } from 'module-loader!./template.mjs';
 
-export { NAME, ROOT_CLASS, ROOT_HTML };
+export { NAME, ROOT_CLASS, PORT_CLASS, ROOT_HTML };
 export const template = {
   NAME, HTML: ROOT_HTML, CSS,
 };
