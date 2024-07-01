cf2042041e3e7689974a4026f466a7d7c2ec46c4
nxSerhey <yakubin.serhey@gmail.com>
2024-07-02 01:12:56 +0300

    Load_Drop_File

--- src/control/Loading/template.mjs
+++ src/control/Loading/template.mjs
@@ -45,7 +45,7 @@ export const CSS = `
   width: 100%;
   height: 100%;
   max-width: 160px;
-  min-width: auto;
+  min-width: 160px;
   background-image: ${MAIN_IMG};
   background-repeat: no-repeat;
   background-size: contain;
