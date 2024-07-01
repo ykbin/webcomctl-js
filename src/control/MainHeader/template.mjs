bac96880acc2253a312749af507f3c9063e157e6
nxSerhey <yakubin.serhey@gmail.com>
2024-07-02 01:38:47 +0300

    Headr

--- src/control/MainHeader/template.mjs
+++ src/control/MainHeader/template.mjs
@@ -37,14 +37,24 @@ ${DARKMODE_SELECTOR_VALUE}
   display: flex;
   height: 43px;
   padding: 5px 10px;
+  z-index: 1;
   border-bottom: 1px solid var(--uic-mhdr-bor);
   box-shadow: 0px 1.6px 3.6px var(--uic-mhdr-bs1), 0px 0px 2.9px var(--uic-mhdr-bs2);
-  box-sizing: border-box;
-  z-index: 1;
   background-color: var(--uic-mhdr-bg);
   overflow: hidden;
   user-select: none;
+  box-sizing: border-box;
   flex-shrink: 0;
+  container-name: header;
+  container-type: inline-size;
+}
+
+@container header (width < 550px)
+{
+  .${CLASS.ROOT} > *:nth-child(1)
+  {
+    display: none;
+  }
 }
 
 @media (device-width < 550px)
