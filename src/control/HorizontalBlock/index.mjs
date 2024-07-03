b1dae3bf769b7a7523532cfe19ba981e3c7c78a9
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-07-03 19:17:20 +0100

    Maker for HorizontalBlock

--- src/control/HorizontalBlock/index.mjs
+++ src/control/HorizontalBlock/index.mjs
@@ -1,5 +1,5 @@
 import { BaseControl } from "webnetq-js";
-import { NAME, ROOT_HTML, PORT_CLASS, CSS } from 'module-loader!./template.mjs';
+import { NAME, ROOT_HTML, ROOT_CLASS, PORT_CLASS, CSS } from 'module-loader!./template.mjs';
 
 export { NAME, ROOT_CLASS, PORT_CLASS, ROOT_HTML };
 export const template = {
