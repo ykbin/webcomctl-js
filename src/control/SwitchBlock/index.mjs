9cb3fb7c1fadd1a36151aa343f565da8872474b1
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-07-14 11:18:00 +0100

    Fix SwitchBlock

--- src/control/SwitchBlock/index.mjs
+++ src/control/SwitchBlock/index.mjs
@@ -1,5 +1,5 @@
 import { BaseControl } from 'webnetq-js';
-import { NAME, ROOT_HTML, ROOT_CLASS, PORT_CLASS, NTH2_CLASS, CSS } from 'module-loader!./template.mjs';
+import { NAME, ROOT_HTML, ROOT_CLASS, PORT_CLASS, NTH1_CLASS, NTH2_CLASS, CSS } from 'module-loader!./template.mjs';
 
 export { NAME, ROOT_CLASS, PORT_CLASS, ROOT_HTML };
 export const template = {
