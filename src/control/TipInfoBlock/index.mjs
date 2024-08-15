42a7f9fb2478633e61d637981ee67080acb8cccd
Serhey Yakubin <yakubin.serhey@gmail.com>
2024-08-15 20:38:21 +0300

    Info content (#58)
    * InfoContent

---
+++ src/control/TipInfoBlock/index.mjs
@@ -0,0 +1,7 @@
+import { BaseControl } from 'webnetq-js';
+import { ROOT_CLASS } from 'module-loader!./template.mjs';
+
+export default class UITipInfoBlockControl extends BaseControl {
+  _init() {
+  }
+};
