4ae701d23f4fdac527fcdf8bf3aee54edda1e3e2
nxSerhey <yakubin.serhey@gmail.com>
2024-08-17 09:40:00 +0300

    info

--- src/control/TipInfoBlock2/index.mjs
+++
@@ -1,7 +0,0 @@
-import { BaseControl } from 'webnetq-js';
-import { ROOT_CLASS } from 'module-loader!./template.mjs';
-
-export default class UITipInfoBlock2Control extends BaseControl {
-  _init() {
-  }
-};
