c060b86b1ef90b881f560911ca38691865787444
nxSerhey <yakubin.serhey@gmail.com>
2024-08-17 09:33:52 +0300

    info

---
+++ src/control/TipInfoBlock2/index.mjs
@@ -0,0 +1,7 @@
+import { BaseControl } from 'webnetq-js';
+import { ROOT_CLASS } from 'module-loader!./template.mjs';
+
+export default class UITipInfoBlock2Control extends BaseControl {
+  _init() {
+  }
+};
---
+++ src/control/TipInfoBlock2/template.mjs
@@ -0,0 +1,215 @@
+import ControlMaker from '../../lib/ControlMaker.mjs';
+import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
+import { UIC_CONTENT_BACKGROUND_COLOR } from '../../lib/WickedTheme.mjs';
+import { UIC_CONTENT_BACKGROUND_COLOR_DARK } from '../../lib/WickedTheme.mjs';
+
+const mk = new ControlMaker('TipInfoBlock2', import.meta.url);
+
+export const ROOT_CLASS = mk.newClassName("Root");
+
+export const CLOSE_CLASS = mk.newClassName("Close");
+
+export const DESCRIPTION_CLASS = mk.newClassName("Description");
+export const PULL_OUT_RIGHT = mk.newClassName("Pull_Out_Right");
+export const PULL_OUT_LEFT = mk.newClassName("Pull_Out_Left");
+export const LIST_CLASS = mk.newClassName("List");
+export const SIZE_CLASS = mk.newClassName("Size");
+
+const CLOSE_IMG = await mk.loadSvgAsCssUrl('./X.svg');
+
+export const ROOT_HTML = `
+<div class="${ROOT_CLASS}">
+  <div>
+    <div class="${DESCRIPTION_CLASS} ${PULL_OUT_RIGHT}">
+      <div>
+        <h2><span class="notranslate" translate="no">JPEG</span>:</h2>
+        <span>
+          One of the popular raster graphics formats used to store photographs and similar images. The JPEG algorithm allows you to compress an image both lossy and lossless (lossless JPEG compression mode).
+        </span>
+        <div class="${LIST_CLASS} notranslate" translate="no">
+          <span><h3>Size</h3><label>88 KB</label></span>
+          <span><h3>Library</h3><label>libjpeg-turbo-2.1.91</label></span>
+          <span><h3>MD5</h3><label>6C539ACE23ECEA28A66FB18514D86A8C</label></span>
+        </div>
+
+          <ul class="${SIZE_CLASS} notranslate" translate="no">
+            <li><h3>Name:</h3><label>Name1234567890</label></li>
+            <li><h3>Height</h3><label>470 px</label></li>
+            <li><h3>JFIF Ver</h3><label>1.1</label></li>
+            <li><h3>ColorSpace</h3><label>YCbCr</label></li>
+          </ul>
+        </div>
+        <div class="${CLOSE_CLASS}"></div>
+    </div>
+  </div>
+`;
+
+export const CSS = `
+
+:root
+{
+  --uic-imgcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR};
+  --menu-bg: white;
+  --menu-col: black;
+  --menu-title-col: #272626;
+  --close-hov: #f95d5d;
+}
+
+${DARKMODE_SELECTOR_VALUE}
+{
+  --uic-imgcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR_DARK};
+  --menu-bg: rgb(23, 23, 26);
+  --menu-col: #b8b4b4;
+  --menu-title-col: #9b9b9b;
+}
+
+.${CLOSE_CLASS}
+{
+  width: 20px;
+  height: 20px;
+  border-radius: 5px;
+  background-image: ${CLOSE_IMG};
+  background-size: 75%;
+  background-position: center;
+  background-repeat: no-repeat;
+  flex-shrink: 0;
+}
+
+.${CLOSE_CLASS}:hover
+{
+  background-color: var(--close-hov);
+}
+
+.${ROOT_CLASS}
+{
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.${ROOT_CLASS} > div
+{
+  display: flex;
+  justify-content: flex-end;
+  width: 0px;
+  height: 100%;
+}
+
+
+.${DESCRIPTION_CLASS} > div > h2
+{
+  display: inline-block;
+  padding-left: 5px;
+  margin: 0px;
+  font-weight: 500;
+  font-size: 18px;
+  color: var(--menu-title-col);
+}
+
+h3
+{
+  margin: 0px;
+  font-weight: 400;
+  font-size: 16px;
+}
+
+.${DESCRIPTION_CLASS}
+{
+  position: absolute;
+  display: flex;
+  max-width: 450px;
+  min-width: 300px;
+  width: 100%;
+  height: calc(100% - 10px);
+  margin-top: 10px;
+  padding: 0px 10px 10px 10px;
+  color: #393939;
+  font-family: Open Sans, Arial, sans-serif;
+  box-sizing: border-box;
+  transform: translateX(0);
+  transition: transform 0.2s ease-in-out;
+  background-color: var(--menu-bg);
+  color: var(--menu-col);
+}
+
+.${PULL_OUT_RIGHT}
+{
+  right: 0;
+  transform: translateX(100%);
+}
+
+.${PULL_OUT_LEFT}
+{
+  left: 0;
+  transform: translateX(-100%);
+}
+
+.${DESCRIPTION_CLASS} *
+{
+  box-sizing: border-box;
+}
+
+.${DESCRIPTION_CLASS} > div > span
+{
+  display: block;
+  max-width: 500px;
+  padding: 0px 5px;
+  margin-bottom: 16px;
+  line-height: 1.7;
+}
+
+.${LIST_CLASS}
+{
+  display: table;
+  width: 100%;
+  border-spacing: 0 10px;
+}
+
+.${LIST_CLASS} span 
+{
+  display: table-row-group;
+}
+
+.${LIST_CLASS} > span > h3
+{
+  display: table-cell;
+  padding-left: 10px;
+  min-width: 50px;
+}
+
+.${LIST_CLASS} > span > label
+{
+  display: table-cell;
+  width: 100%;
+  padding-left: 10px;
+  word-break: break-all;
+}
+
+.${SIZE_CLASS}
+{
+  display: table;
+  border-spacing: 0 10px;
+  margin: 0;
+  padding: 0;
+}
+
+.${SIZE_CLASS} > li
+{
+  display: table-row-group;
+}
+
+.${SIZE_CLASS} > li > h3
+{
+  display: table-cell;
+  padding-left: 10px;
+  min-width: 50px;
+}
+
+.${SIZE_CLASS} > li > label
+{
+  display: table-cell;
+  width: 100%;
+  word-break: break-all;
+  padding-left: 10px;
+}
+`;
