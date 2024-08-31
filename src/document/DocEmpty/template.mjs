316e4fb33a67985f8a14e797608e13107cc777ee
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-08-31 13:31:05 +0100

    CntSmUploadButton

--- src/document/DocEmpty/template.mjs
+++ src/document/DocEmpty/template.mjs
@@ -2,14 +2,13 @@ import ControlMaker from '../../lib/ControlMaker.mjs';
 
 const mk = new ControlMaker('DocEmpty', import.meta.url);
 
-const ROOT_CLASS = mk.newClassName("ROOT_CLASS");
-const PORT_CLASS = mk.newClassName(ROOT_CLASS);
+const PORT_CLASS = mk.newClassName("PORT_CLASS");
 
 mk.newHTML('ROOT_HTML', `
 <!DOCTYPE html>
 <html>
   <head></head>
-  <body class="${ROOT_CLASS}"></body>
+  <body class="${PORT_CLASS}"></body>
 </html>
 `);
 
