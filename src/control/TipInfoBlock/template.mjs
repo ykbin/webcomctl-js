e6eb1df14aae4ae1128902d5546c1943e3ca19d6
nxSerhey <yakubin.serhey@gmail.com>
2024-09-01 00:41:43 +0300

    Fix TIB

--- src/control/TipInfoBlock/template.mjs
+++ src/control/TipInfoBlock/template.mjs
@@ -25,26 +25,9 @@ const CLOSE_HOV_COL = mk.newCSSVariable("CLOSE_HOV_COL", '#ff00005e');
 export const ROOT_HTML = mk.newHTML('ROOT_HTML', `
   <div class="${ROOT_CLASS}">
     <div>
-      <div class="${PORT_CLASS}">
-        <h2><span class="notranslate" translate="no">JPEG</span>:</h2>
-        <span>
-          One of the popular raster graphics formats used to store photographs and similar images. The JPEG algorithm allows you to compress an image both lossy and lossless (lossless JPEG compression mode).
-        </span>
-        <div class="${LIST_CLASS} notranslate" translate="no">
-          <span><h3>Size</h3><label>88 KB</label></span>
-          <span><h3>Library</h3><label>libjpeg-turbo-2.1.91</label></span>
-          <span><h3>MD5</h3><label>6C539ACE23ECEA28A66FB18514D86A8C</label></span>
-        </div>
-
-          <ul class="${SIZE_CLASS} notranslate" translate="no">
-            <li><h3>Name:</h3><label>Name1234567890</label></li>
-            <li><h3>Height</h3><label>470 px</label></li>
-            <li><h3>JFIF Ver</h3><label>1.1</label></li>
-            <li><h3>ColorSpace</h3><label>YCbCr</label></li>
-          </ul>
-        </div>
-        <div class="${CLOSE_CLASS}"></div>
-      </div>
+      <div class="${PORT_CLASS}"></div>
+      <div class="${CLOSE_CLASS}"></div>
+    </div>
   </div>
 `);
 
@@ -72,12 +55,6 @@ ${DARKMODE_SELECTOR_VALUE}
   box-sizing: border-box;
 }
 
-.${ROOT_CLASS} h3
-{
-  margin: 0px;
-  font-weight: 400;
-  font-size: 16px;
-}
 
 .${ROOT_CLASS}
 {
@@ -102,16 +79,6 @@ ${DARKMODE_SELECTOR_VALUE}
   width: 450px;
   height: 100%;
   padding: 10px 20px 10px 20px;
-}
-
-.${ROOT_CLASS} > div > h2
-{
-  display: inline-block;
-  padding-left: 5px;
-  margin: 0px;
-  font-weight: 500;
-  font-size: 18px;
-  color: ${MENU_TITLE_COL.asVar()};
 }
 
 .${PULL_OUT_RIGHT}
@@ -141,7 +108,6 @@ ${DARKMODE_SELECTOR_VALUE}
   transition: width 0.2s;
 }
 
-
 .${ROOT_CLASS} > div > span
 {
   display: block;
@@ -149,61 +115,6 @@ ${DARKMODE_SELECTOR_VALUE}
   padding: 0px 5px;
   margin-bottom: 16px;
   line-height: 1.7;
-}
-
-.${LIST_CLASS}
-{
-  display: table;
-  width: 100%;
-  border-spacing: 0 10px;
-}
-
-.${LIST_CLASS} span 
-{
-  display: table-row-group;
-}
-
-.${LIST_CLASS} > span > h3
-{
-  display: table-cell;
-  padding-left: 10px;
-  min-width: 50px;
-}
-
-.${LIST_CLASS} > span > label
-{
-  display: table-cell;
-  width: 100%;
-  padding-left: 10px;
-  word-break: break-all;
-}
-
-.${SIZE_CLASS}
-{
-  display: table;
-  border-spacing: 0 10px;
-  margin: 0;
-  padding: 0;
-}
-
-.${SIZE_CLASS} > li
-{
-  display: table-row-group;
-}
-
-.${SIZE_CLASS} > li > h3
-{
-  display: table-cell;
-  padding-left: 10px;
-  min-width: 50px;
-}
-
-.${SIZE_CLASS} > li > label
-{
-  display: table-cell;
-  width: 100%;
-  word-break: break-all;
-  padding-left: 10px;
 }
 
 .${CLOSE_CLASS}
