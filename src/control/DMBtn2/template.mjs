37015dcc2a934644471b13815a165da19138762e
Yurii Yakubin <119136621+yakubins@users.noreply.github.com>
2024-09-01 20:52:49 +0100

    Removed exports from DMBtn2 and added newCSSVariableMap to ControlMaker (#65)

--- src/control/DMBtn2/template.mjs
+++ src/control/DMBtn2/template.mjs
@@ -4,38 +4,40 @@ import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
 
 const mk = new ControlMaker('DMBtn2', import.meta.url);
 
+const ROOT_CLASS = mk.newClassName("ROOT_CLASS");
+
 const MOON_IMG = await mk.loadSvgAsCssUrl('./moon.svg');
 const SUN_IMG = await mk.loadSvgAsCssUrl('./sun.svg');
 
-export const ROOT_CLASS = mk.newClassName("Root");
+const vars = mk.newCSSVariableMap({
+  img:   [ MOON_IMG,  SUN_IMG   ],
+  bgHov: [ '#e4e6e8', '#6a6a6a' ],
+  bsHov: [ '#ffffff', '#000000' ],
+  bor:   [ '#7c7c7c', '#616161' ],
+  bs:    [ '#c8c8c8', '#373737' ],
+});
 
-const IMG_VAR = mk.newVarName("Img");
-const BGHOV_VAR = mk.newVarName("BgHov");
-const BSHOV_VAR = mk.newVarName("BsHov");
-const BOR_VAR = mk.newVarName("Bor");
-const BS_VAR = mk.newVarName("Bs");
-
-export const ROOT_HTML = `
+mk.newHTML('ROOT_HTML', `
 <div class="${ROOT_CLASS}"></div>
-`;
+`);
 
-export const CSS = `
+mk.newCSS('CSS', `
 :root
 {
-  ${IMG_VAR}: ${MOON_IMG};
-  ${BGHOV_VAR}: #e4e6e8;
-  ${BSHOV_VAR}: #ffffff;
-  ${BOR_VAR}: #7c7c7c;
-  ${BS_VAR}: #c8c8c8;
+  ${vars.img.toString(0)};
+  ${vars.bgHov.toString(0)};
+  ${vars.bsHov.toString(0)};
+  ${vars.bor.toString(0)};
+  ${vars.bs.toString(0)};
 }
 
 ${DARKMODE_SELECTOR_VALUE}
 {
-  ${IMG_VAR}: ${SUN_IMG};
-  ${BGHOV_VAR}: #6a6a6a;
-  ${BSHOV_VAR}: #000000;
-  ${BOR_VAR}: #616161;
-  ${BS_VAR}: #373737;
+  ${vars.img.toString(1)};
+  ${vars.bgHov.toString(1)};
+  ${vars.bsHov.toString(1)};
+  ${vars.bor.toString(1)};
+  ${vars.bs.toString(1)};
 }
 
 .${ROOT_CLASS}
@@ -43,17 +45,22 @@ ${DARKMODE_SELECTOR_VALUE}
   height: 32px;
   width: 32px;
   border-radius: 50%;
-  border: 1px solid var(${BOR_VAR});
-  background-image: var(${IMG_VAR});
+  border: 1px solid ${vars.bor.asVar()};
+  background-image: ${vars.img.asVar()};
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
-  box-shadow: inset 0px 0px 3px 0px var(${BS_VAR});
+  box-shadow: inset 0px 0px 3px 0px ${vars.bs.asVar()};
 }
 
 .${ROOT_CLASS}:hover
 {
-  background-color: var(${BGHOV_VAR});
-  box-shadow: inset 0px 0px 6px 0px var(${BSHOV_VAR});
+  background-color: ${vars.bgHov.asVar()};
+  box-shadow: inset 0px 0px 6px 0px ${vars.bsHov.asVar()};
 }
-`;
+`);
+
+export function buildComponent()
+{
+  return mk.buildComponent();
+}
