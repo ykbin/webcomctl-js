c4b9c15ff410c641d502f72b9a20d14ee3728ee1
Yurii Yakubin <119136621+yakubins@users.noreply.github.com>
2024-09-02 21:30:44 +0100

    Show, hide and change the side of the debug panel (#68)

--- src/control/DebugPanel/index.mjs
+++ src/control/DebugPanel/index.mjs
@@ -1,7 +1,22 @@
 import { BaseControl, NQDOM } from 'webnetq-js';
-import { ITEM_HTML, TEXT_CLASS, LIST_CLASS } from 'uictmplt-loader!./template.mjs';
+import { ITEM_HTML, TEXT_CLASS, LIST_CLASS, UP_CLASS, DOWN_CLASS, hideClick, LEFT_CLASS, RIGHT_CLASS, sideClick } from 'uictmplt-loader!./template.mjs';
 
 export default class UIDebugPanelControl extends BaseControl {
+  _init() {
+    const hideClickElm = NQDOM.getElementByClassName(this.element, hideClick);
+    hideClickElm && hideClickElm.addEventListener("click", (event) => {
+      const f = this.element.classList.contains(UP_CLASS);
+      this.element.classList.remove(f ? UP_CLASS : DOWN_CLASS);
+      this.element.classList.add(f ? DOWN_CLASS : UP_CLASS);
+    });
+    const sideClickElm = NQDOM.getElementByClassName(this.element, sideClick);
+    sideClickElm && sideClickElm.addEventListener("click", (event) => {
+      const f = this.element.classList.contains(LEFT_CLASS);
+      this.element.classList.remove(f ? LEFT_CLASS : RIGHT_CLASS);
+      this.element.classList.add(f ? RIGHT_CLASS : LEFT_CLASS);
+    });
+  }
+
   setButton(name, params) {
     const itemElm = NQDOM.createElement(ITEM_HTML);
     const textElm = NQDOM.getElementByClassName(itemElm, TEXT_CLASS);
