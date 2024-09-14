3feacb214852f80b3cd427dd1a85b5753e3dbdd7
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-09-14 19:26:05 +0100

    ImageInfoPanel

--- src/control/ImageInfoPanel/index.mjs
+++ src/control/ImageInfoPanel/index.mjs
@@ -1,7 +1,33 @@
-import { BaseControl } from 'webnetq-js';
-import { ROOT_CLASS } from 'module-loader!./template.mjs';
+import { BaseControl, NQDOM } from 'webnetq-js';
+import { DESCRIPTION_TITLE, DESCRIPTION_HISTORY, LIST_CLASS, ITEM_HTML, LIST_NAME, LIST_VALUE } from 'uictmplt-loader!./template.mjs';
 
 export default class UIInfoContentControl extends BaseControl {
   _init() {
+    this._titleElm = NQDOM.getElementByClassName(this.element, DESCRIPTION_TITLE);
+    this._descriptionElm = NQDOM.getElementByClassName(this.element, DESCRIPTION_HISTORY);
+    this._listElm = NQDOM.getElementByClassName(this.element, LIST_CLASS);
+  }
+
+  get title() {
+    return this._titleElm?.textContent;
+  }
+  set title(value) {
+    this._titleElm?.textContent = value;
+  }
+
+  get description() {
+    return this._descriptionElm?.textContent;
+  }
+  set description(value) {
+    this._descriptionElm?.textContent = value;
+  }
+
+  addListItem(name, value) {
+    const itemElm = NQDOM.createElement(ITEM_HTML);
+    const nameElm = NQDOM.getElementByClassName(itemElm, LIST_NAME);
+    nameElm?.textContent = name;
+    const valueElm = NQDOM.getElementByClassName(itemElm, LIST_VALUE);
+    valueElm?.textContent = value;
+    this._listElm?.appendChild(itemElm);
   }
 };
--- src/control/ImageInfoPanel/template.mjs
+++ src/control/ImageInfoPanel/template.mjs
@@ -1,114 +1,80 @@
 import ControlMaker from '../../lib/ControlMaker.mjs';
 import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
-import { UIC_CONTENT_BACKGROUND_COLOR } from '../../lib/WickedTheme.mjs';
-import { UIC_CONTENT_BACKGROUND_COLOR_DARK } from '../../lib/WickedTheme.mjs';
 
-const mk = new ControlMaker('InfoContent', import.meta.url);
+const mk = new ControlMaker('ImageInfoPanel', import.meta.url);
 
-export const ROOT_CLASS = mk.newClassName("Root");
-export const DESCRIPTION_CLASS = mk.newClassName("Description");
-export const DESCRIPTION_FORMAT_CLASS = mk.newClassName("Description_Format");
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",
+  "DESCRIPTION_CLASS", // TODO: Remove
+  "DESCRIPTION_FORMAT_CLASS", // TODO: Remove
+  "DESCRIPTION_TITLE",
+  "DESCRIPTION_HISTORY",
+  "LIST_CLASS",
+  "LIST_NAME",
+  "LIST_VALUE",
+]);
 
-export const DESCRIPTION_TITLE = mk.newClassName("Description_Title");
-export const DESCRIPTION_HISTORY = mk.newClassName("Description_history");
-
-export const LIST_CLASS = mk.newClassName("List");
-export const LIST_PROPERTY = mk.newClassName("List_Property");
-export const LIST_NAME = mk.newClassName("List_Name");
-export const LIST_VALUE = mk.newClassName("List_Value");
-
-export const SIZE_CLASS = mk.newClassName("Size");
-export const SIZE_PROPERTY = mk.newClassName("Size_Property");
-export const SIZE_NAME = mk.newClassName("Size_Name");
-export const SIZE_VALUE = mk.newClassName("Size_Value");
+const vars = mk.newCSSVariableMap({
+  menuCol: [ 'black', '#b8b4b4' ],
+  titleCol: [ '#272626', '#9b9b9b' ],
+});
 
 const SCROLLBAR_THUMB_COLOR = '#b5b5b5c7';
 const SCROLLBAR_TRACK_COLOR = 'transparent';
 
-export const ROOT_HTML = `
-  <div class="${ROOT_CLASS}">
-    <div class="${DESCRIPTION_CLASS}">
+mk.newHTML('ROOT_HTML', `
+  <div class="${clss.ROOT_CLASS}">
+    <div class="${clss.DESCRIPTION_CLASS} notranslate" translate="no">
 
-        <div class="${DESCRIPTION_FORMAT_CLASS}">
-          <h2 class="${DESCRIPTION_TITLE} notranslate" translate="no"><span>JPEG</span>:</h2>
-          <span class="${DESCRIPTION_HISTORY}">
-            One of the popular raster graphics formats used to store photographs and similar images. The JPEG algorithm allows you to compress an image both lossy and lossless (lossless JPEG compression mode).
-          </span>
-        </div>
-        <div class="${LIST_CLASS} notranslate" translate="no">
-          <span class="${LIST_PROPERTY}">
-            <h3 class="${LIST_NAME}">Size</h3>
-            <label class="${LIST_VALUE}">88 KB</label>
-          </span>
-          <span class="${LIST_PROPERTY}">
-            <h3 class="${LIST_NAME}">Library</h3>
-            <label class="${LIST_VALUE}">libjpeg-turbo-2.1.91</label>
-          </span>
-          <span class="${LIST_PROPERTY}">
-            <h3 class="${LIST_NAME}">MD5</h3>
-            <label class="${LIST_VALUE}">6C539ACE23ECEA28A66FB18514D86A8C</label>
-          </span>
+        <div class="${clss.DESCRIPTION_FORMAT_CLASS}">
+          <h2"><span class="${clss.DESCRIPTION_TITLE}>JPEG</span>:</h2>
+          <span class="${clss.DESCRIPTION_HISTORY}"></span>
         </div>
 
-        <ul class="${SIZE_CLASS} notranslate" translate="no">
-          <li class="${SIZE_PROPERTY}">
-            <h3 class="${SIZE_NAME}">Name:</h3>
-            <label class="${SIZE_VALUE}">Name1234567890</label>
-          </li>
-          <li class="${SIZE_PROPERTY}">
-            <h3 class="${SIZE_NAME}">Height</h3>
-            <label class="${SIZE_VALUE}">470 px</label>
-          </li>
-          <li class="${SIZE_PROPERTY}">
-            <h3 class="${SIZE_NAME}">JFIF Ver</h3>
-            <label class="${SIZE_VALUE}">1.1</label>
-          </li>
-          <li class="${SIZE_PROPERTY}">
-            <h3 class="${SIZE_NAME}">ColorSpace</h3>
-            <label class="${SIZE_VALUE}">YCbCr</label>
-          </li>
-        </ul>
+        <div class="${clss.LIST_CLASS}"></div>
 
     </div>
   </div>
-`;
+`);
 
-export const CSS = `
+mk.newHTML('ITEM_HTML', `
+<span>
+  <h3 class="${clss.LIST_NAME}"></h3>
+  <label class="${clss.LIST_VALUE}"></label>
+</span>
+`);
+
+mk.newCSS('CSS', `
 
 :root
 {
-  --uic-imgcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR};
-  --menu-col: black;
-  --menu-title-col: #272626;
-  --close-hov: #80808042;
+  ${vars.toString(0)};
 }
 
 ${DARKMODE_SELECTOR_VALUE}
 {
-  --uic-imgcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR_DARK};
-  --menu-col: #b8b4b4;
-  --menu-title-col: #9b9b9b;
+  ${vars.toString(1)};
 }
 
-.${DESCRIPTION_CLASS}::-webkit-scrollbar
+.${clss.DESCRIPTION_CLASS}::-webkit-scrollbar
 {
   width: 10px;
   height: 10px;
 }
 
-.${DESCRIPTION_CLASS}::-webkit-scrollbar-thumb
+.${clss.DESCRIPTION_CLASS}::-webkit-scrollbar-thumb
 {
   background-color: ${SCROLLBAR_THUMB_COLOR};
   border-radius: 10px;
 }
 
-.${DESCRIPTION_CLASS}::-webkit-scrollbar-track,
-.${DESCRIPTION_CLASS}::-webkit-scrollbar-corner
+.${clss.DESCRIPTION_CLASS}::-webkit-scrollbar-track,
+.${clss.DESCRIPTION_CLASS}::-webkit-scrollbar-corner
 {
   background-color: ${SCROLLBAR_TRACK_COLOR};
 }
 
-.${ROOT_CLASS}
+.${clss.ROOT_CLASS}
 {
   position: relative;
   display: block;
@@ -116,24 +82,24 @@ ${DARKMODE_SELECTOR_VALUE}
   height: 100%;
 }
 
-.${DESCRIPTION_FORMAT_CLASS} h2
+.${clss.DESCRIPTION_FORMAT_CLASS} h2
 {
   display: inline-block;
   padding-left: 5px;
   margin: 0px;
   font-weight: 500;
   font-size: 18px;
-  color: var(--menu-title-col);
+  color: ${vars.titleCol.asVar()};
 }
 
-.${DESCRIPTION_CLASS} h3
+.${clss.DESCRIPTION_CLASS} h3
 {
   margin: 0px;
   font-weight: 400;
   font-size: 16px;
 }
 
-.${DESCRIPTION_CLASS}
+.${clss.DESCRIPTION_CLASS}
 {
   width: 100%;
   height: 100%;
@@ -142,16 +108,16 @@ ${DARKMODE_SELECTOR_VALUE}
   color: #393939;
   font-family: Open Sans, Arial, sans-serif;
   box-sizing: border-box;
-  color: var(--menu-col);
+  color: ${vars.menuCol.asVar()};
   overflow: auto;
 }
 
-.${DESCRIPTION_CLASS} *
+.${clss.DESCRIPTION_CLASS} *
 {
   box-sizing: border-box;
 }
 
-.${DESCRIPTION_FORMAT_CLASS} > span
+.${clss.DESCRIPTION_FORMAT_CLASS} > span
 {
   display: block;
   padding: 0px 5px;
@@ -159,58 +125,35 @@ ${DARKMODE_SELECTOR_VALUE}
   line-height: 1.7;
 }
 
-.${LIST_CLASS}
+.${clss.LIST_CLASS}
 {
   display: table;
   width: 100%;
   border-spacing: 0 10px;
 }
 
-.${LIST_CLASS} span 
+.${clss.LIST_CLASS} span 
 {
   display: table-row-group;
 }
 
-.${LIST_CLASS} > span > h3
+.${clss.LIST_CLASS} > span > h3
 {
   display: table-cell;
   padding-left: 10px;
   min-width: 50px;
 }
 
-.${LIST_CLASS} > span > label
+.${clss.LIST_CLASS} > span > label
 {
   display: table-cell;
   width: 100%;
   padding-left: 10px;
   word-break: break-all;
 }
+`);
 
-.${SIZE_CLASS}
+export function buildComponent()
 {
-  display: table;
-  border-spacing: 0 10px;
-  margin: 0;
-  padding: 0;
+  return mk.buildComponent();
 }
-
-.${SIZE_CLASS} > li
-{
-  display: table-row-group;
-}
-
-.${SIZE_CLASS} > li > h3
-{
-  display: table-cell;
-  padding-left: 10px;
-  min-width: 50px;
-}
-
-.${SIZE_CLASS} > li > label
-{
-  display: table-cell;
-  width: 100%;
-  word-break: break-all;
-  padding-left: 10px;
-}
-`;
