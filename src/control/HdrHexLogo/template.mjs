c3d088d7a959ae34ddbf0b733d46024a0ec1e52e
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-09-14 16:23:36 +0100

    Updates for HdrHexLogo

--- src/control/HdrHexLogo/template.mjs
+++ src/control/HdrHexLogo/template.mjs
@@ -5,34 +5,37 @@ import { HEADER_MOBILE_DEVICE_WIDTH } from '../../lib/WickedTheme.mjs';
 
 const mk = new ControlMaker('HdrHexLogo', import.meta.url);
 
-export const ROOT_CLASS = mk.newClassName("Root");
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",
+]);
 
-const FAVICON1_IMG = await mk.loadSvgAsCssUrl('./favicon1.svg');
-const FAVICON2_IMG = await mk.loadSvgAsCssUrl('./favicon2.svg');
-const HEADER1_IMG = await mk.loadSvgAsCssUrl('./header1.svg');
-const HEADER2_IMG = await mk.loadSvgAsCssUrl('./header2.svg');
+const vars = mk.newCSSVariableMap({
+  favicon: [
+    await mk.loadSvgAsCssUrl('./favicon1.svg'),
+    await mk.loadSvgAsCssUrl('./favicon2.svg'),
+  ],
+  header: [
+    await mk.loadSvgAsCssUrl('./header1.svg'),
+    await mk.loadSvgAsCssUrl('./header2.svg'),
+  ],
+});
 
-const FAVICON_VAR = mk.newVarName("FaviconImg");
-const HEADER_VAR = mk.newVarName("HeaderImg");
-
-export const ROOT_HTML = `
+mk.newHTML('ROOT_HTML', `
 <div class="${ROOT_CLASS}">
   <h3></h3>
   <h2></h2>
 </div>
-`;
+`);
 
-export const CSS = `
+mk.newHTML('CSS', `
 :root
 {
-  ${FAVICON_VAR}: ${FAVICON1_IMG};
-  ${HEADER_VAR}: ${HEADER1_IMG};
+  ${vars.toString(0)};
 }
 
 ${DARKMODE_SELECTOR_VALUE}
 {
-  ${FAVICON_VAR}: ${FAVICON2_IMG};
-  ${HEADER_VAR}: ${HEADER2_IMG};
+  ${vars.toString(1)};
 }
 
 .${ROOT_CLASS} h2,
@@ -91,4 +94,9 @@ ${DARKMODE_SELECTOR_VALUE}
     background-size: 360px;
   }
 }
-`;
+`);
+
+export function buildComponent()
+{
+  return mk.buildComponent();
+}
