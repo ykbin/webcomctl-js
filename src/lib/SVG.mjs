480e14d1293e4433ec3e7657278984a99d18aa23
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-09-15 12:33:20 +0100

    Impl HdrUITLogo

--- src/lib/SVG.mjs
+++ src/lib/SVG.mjs
@@ -3,25 +3,31 @@ import path from 'node:path';
 import { fileURLToPath } from 'url';
 import svgo from 'svgo';
 
-const optimizeOptions = {
-  plugins: [
-    { name: 'removeXMLProcInst', active: true },
-    { name: 'removeDoctype', active: true },
-    { name: 'removeComments', active: true },
-    { name: 'removeXMLNS', active: true },
-    { name: 'cleanupNumericValues', params: { floatPrecision: 2 } },
-    { name: 'convertColors', params: { names2hex: true, rgb2hex: true } },
-    { name: 'removeMetadata', active: true },
-    { name: 'removeTitle', active: true },
-    { name: 'removeDesc', active: true },
-    { name: 'removeEditorsNSData', active: true },
-    { name: 'removeUnusedNS', active: true },
-    { name: 'removeNonInheritableGroupAttrs', active: true },
-    { name: 'collapseGroups', active: true },
-    { name: 'removeDimensions', active: true }, // Remove width and height
-    { name: 'removeViewBox', active: false },
-  ]
-};
+const plugins = [
+  { name: 'removeXMLProcInst', active: true },
+  { name: 'removeDoctype', active: true },
+  { name: 'removeComments', active: true },
+  { name: 'cleanupNumericValues', params: { floatPrecision: 2 } },
+  { name: 'convertColors', params: { names2hex: true, rgb2hex: true } },
+  { name: 'removeMetadata', active: true },
+  { name: 'removeTitle', active: true },
+  { name: 'removeDesc', active: true },
+  { name: 'removeEditorsNSData', active: true },
+  { name: 'removeUnusedNS', active: true },
+  { name: 'removeNonInheritableGroupAttrs', active: true },
+  { name: 'collapseGroups', active: true },
+];
+
+const xmlCssPlugins = [
+  ...plugins,
+];
+
+const htmlPlugins = [
+  ...plugins,
+  { name: 'removeXMLNS', active: true },
+  { name: 'removeDimensions', active: true }, // Remove width and height
+  { name: 'removeViewBox', active: false },
+];
 
 export async function loadSvgAsCssUrlAsync(metaUrl, filename) {
   const currentFilename = fileURLToPath(metaUrl);
@@ -29,7 +35,7 @@ export async function loadSvgAsCssUrlAsync(metaUrl, filename) {
   
   const fname = path.resolve(currentDirname, filename);
   const bytes = await readFile(fname, { encoding: 'utf8', flag: 'r' });
-  const optimizeSvg = svgo.optimize(bytes, optimizeOptions);
+  const optimizeSvg = svgo.optimize(bytes, { plugins: xmlCssPlugins });
   
   const svgUriData1 = 'data:image/svg+xml,' +  encodeURIComponent(optimizeSvg.data);
   const svgUriData2 = 'data:image/svg+xml;base64,' +  Buffer.from(optimizeSvg.data).toString('base64');
@@ -44,7 +50,7 @@ export async function loadSvgAsHtmlAsync(metaUrl, filename) {
   
   const fname = path.resolve(currentDirname, filename);
   const bytes = await readFile(fname, { encoding: 'utf8', flag: 'r' });
-  const optimizeSvg = svgo.optimize(bytes, optimizeOptions);
+  const optimizeSvg = svgo.optimize(bytes, { plugins: htmlPlugins });
 
   return optimizeSvg.data;
 }
