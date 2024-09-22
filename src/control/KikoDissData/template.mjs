06e2365f45828100c55293a8926f83ffd7ab3982
nxSerhey <yakubin.serhey@gmail.com>
2024-09-22 21:27:12 +0300

    Data

---
+++ src/control/KikoDissData/template.mjs
@@ -0,0 +1,50 @@
+import ControlMaker from '../../lib/ControlMaker.mjs';
+
+import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
+
+const mk = new ControlMaker('KikoDiss', import.meta.url);
+
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",
+]);
+
+const vars = mk.newCSSVariableMap({
+  col: [ '#656565', '#4d4d4d' ],
+});
+
+mk.newHTML('ROOT_HTML', `
+<div class="${clss.ROOT_CLASS}">20.10.1979</div>
+`);
+
+mk.newCSS('CSS', `
+:root
+{
+  ${vars.toString(0)};
+}
+
+${DARKMODE_SELECTOR_VALUE}
+{
+  ${vars.toString(1)};
+}
+
+.${clss.ROOT_CLASS}
+{
+  width: inherit;
+  margin: 10px 0;
+  padding: 3px 0;
+  font-size: 14px;
+  font-weight: 600;
+  text-align: center;
+  text-decoration: underline;
+  text-underline-offset: 3px;
+  text-decoration-color: ${vars.col.asVar()};
+  color: ${vars.col.asVar()};
+  font-family: cursive;
+  box-sizing: border-box;
+}
+`);
+
+export function buildComponent()
+{
+  return mk.buildComponent();
+}
