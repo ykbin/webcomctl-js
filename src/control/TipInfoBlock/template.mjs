daa7634742ac2147f29d6661c03029e8e534ac72
nxSerhey <yakubin.serhey@gmail.com>
2024-09-24 23:46:32 +0300

    Butt Info+TipInfo

--- src/control/TipInfoBlock/template.mjs
+++ src/control/TipInfoBlock/template.mjs
@@ -118,11 +118,9 @@ ${DARKMODE_SELECTOR_VALUE}
 .${ROOT_CLASS} > div > span
 {
   display: flex;
-  align-items: center;
   justify-content: flex-end;
   width: 100%;
-  height: 25px;
-  padding-top: 10px;
+  padding-top: 13px;
   flex-shrink: 0;
 }
 
