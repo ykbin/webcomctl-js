e83e7f7842fdb37f5d907ce3b5a77f1db3299307
nxSerhey <yakubin.serhey@gmail.com>
2024-09-30 02:40:02 +0300

    KikoView

--- src/control/KikoDissView/template.mjs
+++ src/control/KikoDissView/template.mjs
@@ -100,14 +100,6 @@ ${DARKMODE_SELECTOR_VALUE}
   background-size: cover;
 }
 
-.${clss.ROOT_CLASS} > span > div + img + div > div
-{
-  border-top: 2px solid;
-  border-right: 2px solid;
-  border-bottom: none;
-  border-left: none;
-}
-
 .${clss.ROOT_CLASS} > span > img
 {
   height: auto;
