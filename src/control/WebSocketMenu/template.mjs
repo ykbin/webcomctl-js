91f09d221d2fa30804d8a6bd35847cf70cc4f1f4
Serhey Yakubin <yakubin.serhey@gmail.com>
2024-10-05 00:42:15 +0300

    Websocket_menu+Butt+Main (#105)

--- src/control/WebSocketMenu/template.mjs
+++ src/control/WebSocketMenu/template.mjs
@@ -3,11 +3,10 @@ import ControlMaker from '../../lib/ControlMaker.mjs';
 import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
 
 const mk = new ControlMaker('WEBSOCKET', import.meta.url);
+export const PORT_CLASS = mk.newClassName("PORT_CLASS");
 
 const clss = mk.newClassNameMap([
   "ROOT_CLASS",
-  "BUTTON_CLASS",
-  "BLOCKING_CLASS",
   "RPANEL_PARAMS_INIT",
   "RPANEL_PARAMS_SHOW",
   "RPANEL_PARAMS_HIDE",
@@ -15,24 +14,13 @@ const clss = mk.newClassNameMap([
 
 const vars = mk.newCSSVariableMap({
   nav_but_bg: [ 'white', 'rgb(23, 23, 26)' ],
-  nav_but_bort: [ '#f3f0f0', '#c2c2c240' ],
-  nav_but_borb: [ '#f3f0f0', '#c2c2c240' ],
-  nav_but_borl: [ '#e2e2e2', '#c2c2c240' ],
-  nav_but_bs: [ '-5px 0 5px -5px #cfcfcf', '-5px 0 5px -5px #cfcfcf' ],
-  nav_but_hov: [ 'radial-gradient(#e1e1e1, white 70%)', 'radial-gradient(#3d3d3d, rgb(23, 23, 26) 70%)' ],
   wsock_rpanel_bor: [ '#f3f0f0', ' #c2c2c240' ],
-  wsock_params: [ 'url(/websocket/info.svg)', 'url(/websocket/info_dark.svg)' ],
 });
 
 mk.newHTML('ROOT_HTML', `
   <div id="wsock-rpanel-params-state" class="${clss.ROOT_CLASS} ${clss.RPANEL_PARAMS_INIT}">
-    <nav class="${clss.BUTTON_CLASS} ${clss.BLOCKING_CLASS}">
-      <div id="wsock-rpanel-button" class="">
-        <div></div>
-      </div>
-    </nav>
     <div>
-      <span id="wsock-rpanel-vlist"></span>
+      <span id="wsock-rpanel-vlist"><div class="${PORT_CLASS}"></div></span>
     </div>
 
   </div>
@@ -49,7 +37,7 @@ ${DARKMODE_SELECTOR_VALUE}
   ${vars.toString(1)};
 }
 
-div.${clss.RPANEL-PARAMS_INIT} > div  > span > div,
+div.${clss.RPANEL_PARAMS_INIT} > div  > span > div,
 .${clss.RPANEL_PARAMS_HIDE} > div > span > div h5
 {
   display: none;
@@ -72,56 +60,6 @@ div.${clss.RPANEL_PARAMS_SHOW}
 {
   display: flex;
   flex-direction: row-reverse;
-}
-
-nav.${clss.BUTTON_CLASS}
-{
-  display: flex;
-  align-items: flex-end;
-  flex-direction: column;
-  height: inherit;
-  width: 0px;
-}
-
-nav.${clss.BUTTON_CLASS} > div
-{
-  background-color: ${vars.nav_bu_bg.asVar()};
-  margin-top: 115px;
-  padding: 20px 5px 20px 5px;
-  box-shadow: ${vars.nav_but_bs.asVar()};
-  border-top: 1px solid ${vars.nav_but_bort.asVar()};
-  border-bottom: 1px solid ${vars.nav_but_borb.asVar()};
-  border-left: 1px solid ${vars.nav_but_borl.asVar()};
-  border-start-start-radius: 5px;
-  border-end-start-radius: 5px;
-}
-
-/*.${clss.BLOCKING_CLASS} > div
-{
-  pointer-events: none;
-}*/
-
-nav.${clss.BUTTON_CLASS} > div.active
-{
-  position: relative;
-  left: 1px;
-  padding: 20px 6px 20px 5px;
-}
-
-nav.${clss.BUTTON_CLASS} > div > div
-{
-  height: 25px;
-  width: 25px;
-  border-radius: 10px;
-  margin-bottom: 5px;
-  background-image: ${vars.wsock_params.asVar()};
-  background-size: contain;
-  background-position: center;
-}
-
-nav.${clss.BUTTON_CLASS} > div:hover
-{
-  background: ${vars.nav_but_hov.asVar()};
 }
 
 .${clss.RPANEL_PARAMS_SHOW}
@@ -318,7 +256,7 @@ nav.${clss.BUTTON_CLASS} > div:hover
   }
 }
 
-@media (device-width < ${COMMON_MOBILE_DEVICE_WIDTH})
+@media (device-width < 550px)
 {
   div.${clss.ROOT_CLASS}
   {
@@ -334,7 +272,7 @@ nav.${clss.BUTTON_CLASS} > div:hover
     top: 140px;
     margin-top: 53px;
   }
-  .${clss.RPANEL-PARAMS_INIT} > div
+  .${clss.RPANEL_PARAMS_INIT} > div
   {
     background: var(--rpanel-init-col);
   }
@@ -349,16 +287,6 @@ nav.${clss.BUTTON_CLASS} > div:hover
   div.${clss.RPANEL_PARAMS_HIDE} > div > span > div
   {
     animation-name: message-value-mob;
-  }
-  nav.${clss.BUTTON_CLASS} > div
-  {
-    padding: 30px 12px 30px 10px;
-    margin-top: 230px;
-  }
-  nav.${clss.BUTTON_CLASS} > div > div
-  {
-    height: 60px;
-    width: 60px;
   }
 }
 `);
