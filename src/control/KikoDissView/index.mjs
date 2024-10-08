c52a98ed47524fd368f29217e16cd90b8003c5dc
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-10-08 19:58:15 +0100

    Added JS to KikoDissView

--- src/control/KikoDissView/index.mjs
+++ src/control/KikoDissView/index.mjs
@@ -8,6 +8,10 @@ export default class UIKikoDissViewControl extends BaseControl {
   _init() {
     this._visible = this.element.classList.contains(SHOW_CLASS);
     this._imageElm = NQDOM.getElementByClassName(this.element, IMAGE_CLASS);
+    this.element.addEventListener("click", event => {
+      if (this._visible && event.target.tagName !== "IMG")
+        this.visible = false;
+    });
   }
 
   get visible() {
