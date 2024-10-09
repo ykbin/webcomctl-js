a5b430b328d2a1f3532f3e0180628bef7d0d64f4
Serhey Yakubin <yakubin.serhey@gmail.com>
2024-10-09 21:58:32 +0300

    RightSideProperties updates (#109)

--- src/control/RightSideProperties/template.mjs
+++ src/control/RightSideProperties/template.mjs
@@ -7,10 +7,8 @@ const mk = new ControlMaker('RightSideProperties', import.meta.url);
 const clss = mk.newClassNameMap([
   "ROOT_CLASS",
   "PORT_CLASS",
-  "RPANEL_PARAMS_INIT",
   "ANIME",
   "SHOW",
-  "HIDE",
 ]);
 
 const vars = mk.newCSSVariableMap({
@@ -19,9 +17,9 @@ const vars = mk.newCSSVariableMap({
 });
 
 mk.newHTML('ROOT_HTML', `
-  <div class="${clss.ROOT_CLASS} ${clss.RPANEL_PARAMS_INIT}">
+  <div class="${clss.ROOT_CLASS}">
     <div>
-      <span"><div class="${clss.PORT_CLASS}"></div></span>
+      <span><div class="${clss.PORT_CLASS}"></div></span>
     </div>
   </div>
 `);
@@ -37,8 +35,7 @@ ${DARKMODE_SELECTOR_VALUE}
   ${vars.toString(1)};
 }
 
-.${clss.ROOT_CLASS} > div  > span > div,
-.${clss.RPANEL_PARAMS_HIDE} > div > span > div h5
+.${clss.PORT_CLASS}
 {
   display: none;
 }
@@ -46,7 +43,7 @@ ${DARKMODE_SELECTOR_VALUE}
 .${clss.ROOT_CLASS}.${clss.SHOW} > div  > span > div,
 .${clss.ROOT_CLASS}.${clss.HIDE} > div  > span > div,
 .${clss.ROOT_CLASS}.${clss.ANIME}.${clss.SHOW} > div  > span > div,
-.${clss.ROOT_CLASS}.${clss.ANIME}.${clss.HIDE} > div  > span > div
+.${clss.ROOT_CLASS}.${clss.ANIME} > div  > span > div
 {
   display: flex;
 }
@@ -88,8 +85,8 @@ div.${clss.SHOW}
 .${clss.ROOT_CLASS} > div  > span > div
 {
   align-items: center;
-  height: 30px;
   padding: 0px 10px 0px 20px;
+  max-width: 200px;
 }
 
 .${clss.ROOT_CLASS} > span > div:last-child
@@ -107,7 +104,7 @@ div.${clss.SHOW}
   padding-left: 5px;
 }
 
-.${clss.ANIME}.${clss.HIDE} > div  > span > div
+.${clss.ANIME} > div  > span > div
 {
   animation-name: message-value;
   animation-fill-mode: forwards;
@@ -119,33 +116,33 @@ div.${clss.SHOW}
 {
   0%
   {
-    width: 200px;
+    width: 100%;
     visibility: visible;
   }
   25%
   {
-    width: 150px;
+    width: 75%;
     visibility: hidden;
   }
   50%
   {
-    width: 100px;
+    width: 50%;
     visibility: hidden;
   }
   75%
   {
-    width: 50px;
+    width: 25%;
     visibility: hidden;
   }
   99%
   {
-    width: 0px;
+    width: 0;
     visibility: hidden;
   }
   100%
   {
     display: none;
-    width: 0px;
+    width: 0;
     visibility: hidden;
   }
 }
@@ -164,102 +161,32 @@ div.${clss.SHOW}
   {
     display: none;
     visibility: hidden;
-    width: 0px;
+    width: 0;
   }
   1%
   {
-    width: 0px;
+    width: 0;
     visibility: hidden;
   }
   25%
   {
-    width: 50px;
+    width: 25%;
     visibility: hidden;
   }
   50%
   {
-    width: 100px;
+    width: 50%;
     visibility: hidden;
   }
   75%
   {
-    width: 150px;
+    width: 75%;
     visibility: hidden;
   }
   100%
   {
-    width: 200px;
+    width: 100%;
     visibility: visible;
-  }
-}
-
-@keyframes message-value-back-mob
-{
-  0%
-  {
-    display: none;
-    visibility: hidden;
-    width: 0px;
-  }
-  1%
-  {
-    width: 0px;
-    visibility: hidden;
-  }
-  25%
-  {
-    width: 50px;
-    visibility: hidden;
-  }
-  50%
-  {
-    width: 100px;
-    visibility: hidden;
-  }
-  75%
-  {
-    width: 200px;
-    visibility: hidden;
-  }
-  100%
-  {
-    width: 400px;
-    visibility: visible;
-  }
-}
-
-@keyframes message-value-mob
-{
-  0%
-  {
-    width: 400px;
-    visibility: visible;
-  }
-  25%
-  {
-    width: 200px;
-    visibility: hidden;
-  }
-  50%
-  {
-    width: 100px;
-    visibility: hidden;
-  }
-  75%
-  {
-    width: 50px;
-    visibility: hidden;
-  }
-  99%
-  {
-    width: 0px;
-    visibility: hidden;
-  }
-  100%
-  {
-    display: none;
-    width: 0px;
-    visibility: hidden;
   }
 }
 
@@ -279,21 +206,13 @@ div.${clss.SHOW}
     top: 140px;
     margin-top: 53px;
   }
-  .${clss.RPANEL_PARAMS_INIT} > div
-  {
-    background: var(--rpanel-init-col);
-  }
   .${clss.ROOT_CLASS} > div > span
   {
     padding-top: 20px;
   }
-  div.${clss.RPANEL_PARAMS_SHOW} > div > span > div
+  div.${clss.ROOT_CLASS} > div  > span > div
   {
-    animation-name: message-value-back-mob;
-  }
-  div.${clss.RPANEL_PARAMS_HIDE} > div > span > div
-  {
-    animation-name: message-value-mob;
+    max-width: 400px;
   }
 }
 `);
