21f6db724335aad588cda9a78f515c8ef62f7b97
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-10-13 10:08:04 +0100

    TextContent updates

--- src/control/TextContent/template.mjs
+++ src/control/TextContent/template.mjs
@@ -8,79 +8,76 @@ import { TOOLBAR_FONT_MONOSPACE } from '../../lib/WickedTheme.mjs';
 
 const mk = new ControlMaker('TextContent', import.meta.url);
 
-export const ROOT_CLASS = mk.newClassName("Root");
-export const NUMBERS_CLASS = mk.newClassName("Numbers");
-export const CONTENT_CLASS = mk.newClassName("Content");
-export const OFFSET_CLASS = mk.newClassName("Offset");
-export const BLUE_CLASS = mk.newClassName("Blue");
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",
+  "NUMBERS",
+  "CONTENT",
+  "OFFSET",
+  "BLUE",
+]);
+
+const vars = mk.newCSSVariableMap({
+  bg: [ UIC_CONTENT_BACKGROUND_COLOR, UIC_CONTENT_BACKGROUND_COLOR_DARK ],
+  col: [ 'black', '#b8b4b4' ],
+  bor: [ '#e6e6e6', '#252525' ],
+  numCol: [ '#fafafa', '#19191d' ],
+});
 
 const BLUE_COLOR = '#0000ff';
 const OFFSET_COLOR = 'rgb(197, 6, 11)';
 const SCROLLBAR_THUMB_COLOR = '#b5b5b5c7';
 const SCROLLBAR_TRACK_COLOR = 'transparent';
-const TEXT_CONTENT_COLOR1 = 'black';
-const TEXT_CONTENT_COLOR2 = '#b8b4b4';
-const NUMBER_BORDER_COLOR1 = '#e6e6e6';
-const NUMBER_BORDER_COLOR2 = '#252525';
-const NUMBER_BACKGROUND_COLOR1 = '#fafafa';
-const NUMBER_BACKGROUND_COLOR2 = '#19191d';
 
-export const ROOT_HTML = `
-<div class="${ROOT_CLASS}">
-  <ul class="${NUMBERS_CLASS}"></ul>
-  <div class="${CONTENT_CLASS}"></div>
+mk.newHTML('ROOT_HTML', `
+<div class="${clss.ROOT_CLASS}">
+  <ul class="${clss.NUMBERS}"></ul>
+  <div class="${clss.CONTENT}"></div>
 </div>
-`;
+`);
 
-export const CSS = `
+mk.newCSS('CSS', `
 :root
 {
-  --uic-txtcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR};
-  --uic-txtcnt-col: ${TEXT_CONTENT_COLOR1};
-  --uic-txtcnt-bor: ${NUMBER_BORDER_COLOR1};
-  --uic-txtcnt-num-col: ${NUMBER_BACKGROUND_COLOR1};
+  ${vars.toString(0)};
 }
 
 ${DARKMODE_SELECTOR_VALUE}
 {
-  --uic-txtcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR_DARK};
-  --uic-txtcnt-col: ${TEXT_CONTENT_COLOR2};
-  --uic-txtcnt-bor: ${NUMBER_BORDER_COLOR2};
-  --uic-txtcnt-num-col: ${NUMBER_BACKGROUND_COLOR2};
+  ${vars.toString(1)};
 }
 
-.${ROOT_CLASS} ul
+.${clss.ROOT_CLASS} ul
 {
   margin: 0px;
   padding: 0px;
   list-style-type: none;
 }
 
-.${ROOT_CLASS} > div::-webkit-scrollbar
+.${clss.ROOT_CLASS} > div::-webkit-scrollbar
 {
   width: 10px;
   height: 10px;
 }
 
-.${ROOT_CLASS} > div::-webkit-scrollbar-thumb
+.${clss.ROOT_CLASS} > div::-webkit-scrollbar-thumb
 {
   background-color: ${SCROLLBAR_THUMB_COLOR};
   border-radius: 10px;
 }
 
-.${ROOT_CLASS} > div::-webkit-scrollbar-track,
-.${ROOT_CLASS} > div::-webkit-scrollbar-corner
+.${clss.ROOT_CLASS} > div::-webkit-scrollbar-track,
+.${clss.ROOT_CLASS} > div::-webkit-scrollbar-corner
 {
   background-color: ${SCROLLBAR_TRACK_COLOR};
 }
 
-.${ROOT_CLASS} > ul::-webkit-scrollbar
+.${clss.ROOT_CLASS} > ul::-webkit-scrollbar
 {
   width: 0;
   height: 10px;
 }
 
-.${ROOT_CLASS}
+.${clss.ROOT_CLASS}
 {
   display: flex;
   height: 100%;
@@ -88,18 +85,18 @@ ${DARKMODE_SELECTOR_VALUE}
   font-size: 14px;
   letter-spacing: 2px;
   line-height: 1.4em;
-  color: var(--uic-txtcnt-col);
+  color: ${vars.col.asVar()};
   font-family: ${TOOLBAR_FONT_MONOSPACE};
-  background-color: var(--uic-txtcnt-bg);
+  background-color: ${vars.bg.asVar()};
   box-sizing: border-box;
 }
 
-.${ROOT_CLASS} > *
+.${clss.ROOT_CLASS} > *
 {
   box-sizing: border-box;
 }
 
-.${ROOT_CLASS} > div
+.${clss.ROOT_CLASS} > div
 {
   height: auto;
   width: 100%;
@@ -108,18 +105,18 @@ ${DARKMODE_SELECTOR_VALUE}
   overflow-x: scroll;
 }
 
-.${ROOT_CLASS} > div > span
+.${clss.ROOT_CLASS} > div > span
 {
   white-space: pre;
 }
 
-.${ROOT_CLASS} > ul
+.${clss.ROOT_CLASS} > ul
 {
   height: auto;
   min-width: 55px;
   padding: 10px 5px 10px 5px;
-  border-right: 1px solid var(--uic-txtcnt-bor);
-  background-color: var(--uic-txtcnt-num-col);
+  border-right: 1px solid ${vars.bor.asVar()};
+  background-color: ${vars.numCol.asVar()};
   text-align: center;
   overflow-y: auto;
   overflow-x: scroll;
@@ -127,38 +124,43 @@ ${DARKMODE_SELECTOR_VALUE}
   flex-shrink: 0;
 }
 
-.${ROOT_CLASS} > ul:empty
+.${clss.ROOT_CLASS} > ul:empty
 {
   display: none;
 }
 
-.${ROOT_CLASS} > ul > li,
-.${ROOT_CLASS} > div > span
+.${clss.ROOT_CLASS} > ul > li,
+.${clss.ROOT_CLASS} > div > span
 {
   display: block;
   height: 20px;
 }
 
-.${BLUE_CLASS}
+.${clss.BLUE}
 {
   color: ${BLUE_COLOR};
 }
 
-.${OFFSET_CLASS}
+.${clss.OFFSET}
 {
   color: ${OFFSET_COLOR};
 }
 
 @media (device-width < ${COMMON_MOBILE_DEVICE_WIDTH})
 {
-  .${ROOT_CLASS}
+  .${clss.ROOT_CLASS}
   {
     font-size: 25px;
   }
-  .${ROOT_CLASS} > ul > li, 
-  .${ROOT_CLASS} > div > span
+  .${clss.ROOT_CLASS} > ul > li, 
+  .${clss.ROOT_CLASS} > div > span
   {
     height: 30px;
   }
 }
-`;
+`);
+
+export function buildComponent()
+{
+  return mk.buildComponent();
+}
