aa924f395e2bcb9821c104074f9567c4602ebdf3
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-10-19 20:59:02 +0100

    Impl ProtocolContent

--- src/control/TripleBlock/template.mjs
+++ src/control/TripleBlock/template.mjs
@@ -2,7 +2,7 @@ import ControlMaker from '../../lib/ControlMaker.mjs';
 
 import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
 
-const mk = new ControlMaker('ProtocolContent', import.meta.url);
+const mk = new ControlMaker('TripleBlock', import.meta.url);
 
 const clss = mk.newClassNameMap([
   "ROOT_CLASS",
