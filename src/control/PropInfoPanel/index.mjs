ff16a1651cacb2e71bd5a6e078104634d9909fc2
nxSerhey <yakubin.serhey@gmail.com>
2024-11-07 21:30:28 +0200

    Prop Info

--- src/control/PropInfoPanel/index.mjs
+++ src/control/PropInfoPanel/index.mjs
@@ -1,37 +1,8 @@
-import { BaseControl, NQDOM } from 'webnetq-js';
-import { DESCRIPTION_TITLE, DESCRIPTION_HISTORY, LIST_CLASS, ITEM_HTML, LIST_NAME, LIST_VALUE } from 'uictmplt-loader!./template.mjs';
+import { BaseControl } from 'webnetq-js';
 
-export default class UIInfoContentControl extends BaseControl {
-  _titleElm;
-  _descriptionElm;
-  _listElm;
+export default class PropInfoPanel extends BaseControl {
 
   _init() {
-    this._titleElm = NQDOM.getElementByClassName(this.element, DESCRIPTION_TITLE);
-    this._descriptionElm = NQDOM.getElementByClassName(this.element, DESCRIPTION_HISTORY);
-    this._listElm = NQDOM.getElementByClassName(this.element, LIST_CLASS);
   }
 
-  get title() {
-    return this._titleElm && this._titleElm.textContent;
-  }
-  set title(value) {
-    this._titleElm && (this._titleElm.textContent = value);
-  }
-
-  get description() {
-    return this._descriptionElm && this._descriptionElm.textContent;
-  }
-  set description(value) {
-    this._descriptionElm && (this._descriptionElm.textContent = value);
-  }
-
-  addListItem(name, value) {
-    const itemElm = NQDOM.createElement(ITEM_HTML);
-    const nameElm = NQDOM.getElementByClassName(itemElm, LIST_NAME);
-    nameElm && (nameElm.textContent = name);
-    const valueElm = NQDOM.getElementByClassName(itemElm, LIST_VALUE);
-    valueElm && (valueElm.textContent = value);
-    this._listElm && this._listElm.appendChild(itemElm);
-  }
 };
