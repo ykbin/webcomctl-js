9dd637f00ccded1d5f6765ea0fe002457350a2da
nxSerhey <yakubin.serhey@gmail.com>
2024-11-07 02:44:12 +0200

    Prop

--- src/control/PropInfoPanel/template.mjs
+++ src/control/PropInfoPanel/template.mjs
@@ -2,7 +2,7 @@ import ControlMaker from '../../lib/ControlMaker.mjs';
 import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
 import { TOOLBAR_FONT_SANS } from '../../lib/WickedTheme.mjs';
 
-const mk = new ControlMaker('ImageInfoPanel', import.meta.url);
+const mk = new ControlMaker('PropInfoPanel', import.meta.url);
 
 const clss = mk.newClassNameMap([
   "ROOT_CLASS",
