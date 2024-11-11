eccf04822af91d66f555df65a989921a8c56c825
nxSerhey <yakubin.serhey@gmail.com>
2024-11-11 08:42:39 +0200

    New butt

--- src/control/ImageContent/template.mjs
+++ src/control/ImageContent/template.mjs
@@ -78,12 +78,12 @@ ${DARKMODE_SELECTOR_VALUE}
 
 .${BUTT_LEFT_CLASS}
 {
-  justify-content: flex-start;
+  justify-content: flex-end;
 }
 
 .${BUTT_RIGHT_CLASS}
 {
-  justify-content: flex-end;
+  justify-content: flex-start;
 }
 
 .${ROOT_CLASS} > div.${BUTT_LEFT_CLASS},
