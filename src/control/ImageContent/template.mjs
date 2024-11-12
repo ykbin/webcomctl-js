a4499b11ec71fe80e251fdd1f927931c5ad6f3de
Serhey Yakubin <yakubin.serhey@gmail.com>
2024-11-12 20:58:17 +0200

    ImageContent (#124)

--- src/control/ImageContent/template.mjs
+++ src/control/ImageContent/template.mjs
@@ -8,10 +8,23 @@ const mk = new ControlMaker('ImageContent', import.meta.url);
 
 export const ROOT_CLASS = mk.newClassName("Root");
 export const CONTENT_CLASS = mk.newClassName("Content");
+export const BUTT_LEFT_CLASS = mk.newClassName("ButtLeft");
+export const BUTT_RIGHT_CLASS = mk.newClassName("ButtRight");
+const PLAY = await mk.loadSvgAsCssUrl('./play.svg');
 
 export const ROOT_HTML = `
 <div class="${ROOT_CLASS}" draggable="false">
+  <div class="${BUTT_LEFT_CLASS}">
+    <div>
+      <div></div>
+    </div>
+  </div>
   <img class="${CONTENT_CLASS}"/>
+  <div class="${BUTT_RIGHT_CLASS}">
+    <div>
+      <div></div>
+    </div>
+  </div>
 </div>
 `;
 
@@ -19,11 +32,15 @@ export const CSS = `
 :root
 {
   --uic-imgcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR};
+  --uic-imgcnt-but: #f3f3f3;
+  --uic-imgcnt-buthov: #bcbcbc;
 }
 
 ${DARKMODE_SELECTOR_VALUE}
 {
   --uic-imgcnt-bg: ${UIC_CONTENT_BACKGROUND_COLOR_DARK};
+  --uic-imgcnt-but: #252525;
+  --uic-imgcnt-buthov: #424242;
 }
 
 .${ROOT_CLASS}
@@ -48,5 +65,61 @@ ${DARKMODE_SELECTOR_VALUE}
   border: 1px solid;
   border-color: #f3f3f3;
   box-sizing: border-box;
+  flex-shrink: 0;
+}
+
+.${ROOT_CLASS} > div
+{
+  display: none;
+  height: 40px;
+  width: 100%;
+}
+
+.${ROOT_CLASS} > div > div
+{
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 20px;
+  background-color: var(--uic-imgcnt-but);
+  border-radius: 5px;
+  flex-shrink: 0;
+}
+
+.${ROOT_CLASS} > div > div:hover
+{
+  background-color: var(--uic-imgcnt-buthov);
+}
+
+.${ROOT_CLASS} > div > div > div
+{
+  height: 20px;
+  width: 20px;
+  background-image: ${PLAY};
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  overflow: hidden;
+}
+
+.${ROOT_CLASS} > div:first-child > div > div
+{
+  transform: scaleX(-1);
+}
+
+.${BUTT_LEFT_CLASS}
+{
+  justify-content: flex-start;
+}
+
+.${BUTT_RIGHT_CLASS}
+{
+  justify-content: flex-end;
+}
+
+.${ROOT_CLASS} > div.${BUTT_LEFT_CLASS},
+.${ROOT_CLASS} > div.${BUTT_RIGHT_CLASS}
+{
+  display: flex;
 }
 `;
