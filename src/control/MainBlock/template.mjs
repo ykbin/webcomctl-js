9ad74a30cb1a331d3e458f6e44c5728aa6411785
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-11-23 11:22:48 +0000

    Maker build component updates

--- src/control/MainBlock/template.mjs
+++ src/control/MainBlock/template.mjs
@@ -2,45 +2,47 @@ import ControlMaker from '../../lib/ControlMaker.mjs';
 
 const mk = new ControlMaker('MainBlock', import.meta.url);
 
-export const ROOT_CLASS = mk.newClassName("Root");
-export const PORT_CLASS = mk.newClassName("Port");
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",
+  "PORT_CLASS",
+]);
 
 const SCTHBG_CLR = '#b5b5b5c7';
 const SCTRBG_CLR = 'transparent';
 
-export const ROOT_HTML = `
-<div class="${ROOT_CLASS}">
-  <div class="${PORT_CLASS}"></div>
+mk.newHTML('ROOT_HTML', `
+<div class="${clss.ROOT_CLASS}">
+  <div class="${clss.PORT_CLASS}"></div>
 </div>
-`;
+`);
 
-export const CSS = `
-.${ROOT_CLASS}::-webkit-scrollbar
+mk.newCSS('CSS', `
+.${clss.ROOT_CLASS}::-webkit-scrollbar
 {
   width: 10px;
   height: 10px;
 }
 
-.${ROOT_CLASS}::-webkit-scrollbar-thumb
+.${clss.ROOT_CLASS}::-webkit-scrollbar-thumb
 {
   background-color: ${SCTHBG_CLR};
   border-radius: 10px;
 }
 
-.${ROOT_CLASS}::-webkit-scrollbar-track,
-.${ROOT_CLASS}::-webkit-scrollbar-corner
+.${clss.ROOT_CLASS}::-webkit-scrollbar-track,
+.${clss.ROOT_CLASS}::-webkit-scrollbar-corner
 {
   background-color: ${SCTRBG_CLR};
 }
 
-.${ROOT_CLASS}
+.${clss.ROOT_CLASS}
 {
   height: 100%;
   width: 100%;
   overflow: auto;
 }
 
-.${ROOT_CLASS} > div
+.${clss.ROOT_CLASS} > div
 {
   display: flex;
   flex-direction: column;
@@ -48,4 +50,9 @@ export const CSS = `
   width: 100%;
   min-width: 685px;
 }
-`;
+`);
+
+export function buildComponent()
+{
+  return mk.buildComponent();
+}
