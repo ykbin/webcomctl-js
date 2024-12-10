a0729e04eeb4cd66c288d59b6476e53710d283e9
nxSerhey <yakubin.serhey@gmail.com>
2024-12-11 01:34:24 +0200

    DBC webcomctl

--- src/control/DBCControl/template.mjs
+++ src/control/DBCControl/template.mjs
@@ -8,9 +8,9 @@ const vars = mk.newCSSVariableMap({
 const clss = mk.newClassNameMap([
   "ROOT_CLASS",
   "PORT_CLASS",
-  "dbc-tree-off",
-  "left-on",
-  "dbc-right-panel"
+  "dbc_tree_off",
+  "left_on",
+  "dbc_right_panel"
 ]);
 
 mk.newHTML('ROOT_HTML', `
@@ -42,24 +42,24 @@ ${DARKMODE_SELECTOR_VALUE}
   box-sizing: border-box;
 }
 
-div.${clss.dbc-tree-off} > div.${clss.ROOT_CLASS} > nav,
-div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS} > nav + div,
-div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS} > nav + div + span
+div.${clss.dbc_tree_off} > div.${clss.ROOT_CLASS} > nav,
+div.${clss.dbc_tree_off}  > div.${clss.ROOT_CLASS} > nav + div,
+div.${clss.dbc_tree_off}  > div.${clss.ROOT_CLASS} > nav + div + span
 {
   display: none;
 }
 
-div.${clss.dbc-tree-off}  > .${clss.dbc-right-panel} 
+div.${clss.dbc_tree_off}  > .${clss.dbc_right_panel} 
 {
   padding: 0px 15px 15px 75px;
 }
 
-div.${clss.dbc-tree-off} 
+div.${clss.dbc_tree_off} 
 {
   grid-template-areas: 'right right';
 }
 
-div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS}
+div.${clss.dbc_tree_off}  > div.${clss.left_on}
 {
   display: block;
   grid-area: right;
@@ -74,33 +74,26 @@ div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS}
   max-width: initial;
 }
 
-div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS} > s > div
-{
-  width: inherit;
-  height: initial;
-  margin: 0;
-}
-
-div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS} > s > div > div
+div.${clss.dbc_tree_off}  > div.${clss.ROOT_CLASS} > s > div > div
 {
   width: 35px;
   height: 30px;
   transition: width 0.250s, height 0.250s;
 }
 
-div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS} > s > div
+div.${clss.dbc_tree_off}  > div.${clss.left_on} > s > div
 {
   width: inherit;
   height: initial;
   margin: 0;
 }
 
-div.${clss.dbc-tree-off}  div.${clss.ROOT_CLASS} > s > div:hover
+div.${clss.dbc_tree_off}  div.${clss.left_on} > s > div:hover
 {
   border-radius: 20px;
 }
 
-div.${clss.dbc-tree-off}  > div.${clss.ROOT_CLASS} > s
+div.${clss.dbc_tree_off}  > div.${clss.left_on} > s
 {
   height: inherit;
   width: inherit;
