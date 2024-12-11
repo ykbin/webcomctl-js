eae32fb70c3c2644688864155d095cf17b434ae5
Yurii Yakubin <119136621+yakubins@users.noreply.github.com>
2024-12-11 16:38:18 +0000

    Click event for VertButtonInfo (#135)

--- src/control/VertButtonInfo/index.mjs
+++ src/control/VertButtonInfo/index.mjs
@@ -1,4 +1,16 @@
-import { BaseControl } from 'webnetq-js';
+import { BaseControl, NQDOM } from 'webnetq-js';
+import { CLICK } from 'uictmplt-loader!./template.mjs';
+
+const CLICK_EVENT = "click";
 
 export default class UIVertButtonInfoControl extends BaseControl {
+  _init() {
+    const clicknElm = NQDOM.getElementByClassName(this.element, CLICK);
+    if (clicknElm) {
+      clicknElm.addEventListener('click', (event) => {
+        this.dispatchEvent(CLICK_EVENT, event);
+      });
+      this.registerEvent(CLICK_EVENT);
+    }
+  }
 };
