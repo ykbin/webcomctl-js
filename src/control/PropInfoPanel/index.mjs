058d9e072aee1d21b034dd89b5fdfef47e68638b
Yurii Yakubin <yurii.yakubin@gmail.com>
2024-12-18 13:12:42 +0000

    Added JS code implementation for PropInfoPanel

--- src/control/PropInfoPanel/index.mjs
+++ src/control/PropInfoPanel/index.mjs
@@ -1,8 +1,39 @@
-import { BaseControl } from 'webnetq-js';
+import { BaseControl, NQDOM } from 'webnetq-js';
+import { TITLE, ITEM_HTML, LIST_CLASS, LIST_NAME, LIST_VALUE } from 'uictmplt-loader!./template.mjs';
 
 export default class PropInfoPanel extends BaseControl {
+  _titleElm;
+  _listElm;
+  _valueElmMap = {};
 
   _init() {
+      this._titleElm = NQDOM.getElementByClassName(this.element, TITLE);
+      this._listElm = NQDOM.getElementByClassName(this.element, LIST_CLASS);
   }
 
+  get title() {
+    return this._titleElm && this._titleElm.textContent;
+  }
+
+  set title(value) {
+    this._titleElm && (this._titleElm.textContent = value);
+  }
+
+  setItem(name, value) {
+    let valueElm = this._valueElmMap[name];
+    if (this._valueElmMap[name]) {
+      valueElm.textContent = value;
+    }
+    else if (this._listElm) {
+      const itemElm = NQDOM.createElement(ITEM_HTML);
+      const nameElm = NQDOM.getElementByClassName(itemElm, LIST_NAME);
+      valueElm = NQDOM.getElementByClassName(itemElm, LIST_VALUE);
+      if (valueElm) {
+        this._valueElmMap[name] = valueElm;
+        valueElm.textContent = value;
+        nameElm && (nameElm.textContent = name);
+        this._listElm.appendChild(itemElm);
+      }
+    }
+  }
 };
