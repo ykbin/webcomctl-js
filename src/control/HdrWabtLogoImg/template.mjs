1532ac54320c9a7701596487d74e84375a16dd58
nxSerhey <yakubin.serhey@gmail.com>
2024-12-23 01:04:53 +0200

    WS Logo separation

--- src/control/HdrWabtLogoImg/template.mjs
+++ src/control/HdrWabtLogoImg/template.mjs
@@ -3,7 +3,7 @@ import ControlMaker from '../../lib/ControlMaker.mjs';
 import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
 import { HEADER_MOBILE_DEVICE_WIDTH } from '../../lib/WickedTheme.mjs';
 
-const mk = new ControlMaker('HdrWabtLogo', import.meta.url);
+const mk = new ControlMaker('HdrWabtLogoFavicon', import.meta.url);
 
 const clss = mk.newClassNameMap([
   "ROOT_CLASS",
@@ -14,17 +14,10 @@ const vars = mk.newCSSVariableMap({
     await mk.loadSvgAsCssUrl('./favicon1.svg'),
     await mk.loadSvgAsCssUrl('./favicon2.svg'),
   ],
-  header: [
-    await mk.loadSvgAsCssUrl('./header1.svg'),
-    await mk.loadSvgAsCssUrl('./header2.svg'),
-  ],
 });
 
 mk.newHTML('ROOT_HTML', `
-<div class="${clss.ROOT_CLASS}">
-  <h3></h3>
-  <h2></h2>
-</div>
+<h3 class="${clss.ROOT_CLASS}"></h3>
 `);
 
 mk.newHTML('CSS', `
@@ -38,29 +31,9 @@ ${DARKMODE_SELECTOR_VALUE}
   ${vars.toString(1)};
 }
 
-.${clss.ROOT_CLASS} h2,
-.${clss.ROOT_CLASS} h3
-{
-  margin: 0px;
-  padding: 0px;
-  font-size: 16px;
-  font-weight: 400;
-}
-
-.${clss.ROOT_CLASS} *
-{
-  box-sizing: border-box;
-}
-
 .${clss.ROOT_CLASS}
 {
-  display: flex;
   height: 33px;
-}
-
-.${clss.ROOT_CLASS} > h3
-{
-  height: 100%;
   width: 35px;
   margin-right: 7px;
   background-repeat: no-repeat;
@@ -69,33 +42,16 @@ ${DARKMODE_SELECTOR_VALUE}
   background-position-x: left;
   background-image: ${vars.favicon.asVar()};
   flex-shrink: 0;
-}
-
-.${clss.ROOT_CLASS} > h2
-{
-  width: 77px;
-  height: 100%;
-  margin-right: 15px;
-  background-size: 180px;
-  background-repeat: no-repeat;
-  background-position-y: center;
-  background-position-x: left;
-  background-image: ${vars.header.asVar()};
-  flex-shrink: 0;
+  margin: 0px;
+  padding: 0px;
+  font-size: 16px;
+  font-weight: 400;
+  box-sizing: border-box;
 }
 
 @media (device-width < ${HEADER_MOBILE_DEVICE_WIDTH})
 {
   .${clss.ROOT_CLASS}
-  {
-    height: 130px;
-  }
-  .${clss.ROOT_CLASS} > h2
-  {
-    width: 170px;
-    background-size: 425px;
-  }
-  .${clss.ROOT_CLASS} > h3
   {
     display: none;
   }
--- src/control/HdrWabtLogoName/template.mjs
+++ src/control/HdrWabtLogoName/template.mjs
@@ -3,17 +3,13 @@ import ControlMaker from '../../lib/ControlMaker.mjs';
 import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
 import { HEADER_MOBILE_DEVICE_WIDTH } from '../../lib/WickedTheme.mjs';
 
-const mk = new ControlMaker('HdrWabtLogo', import.meta.url);
+const mk = new ControlMaker('HdrWabtLogoName', import.meta.url);
 
 const clss = mk.newClassNameMap([
   "ROOT_CLASS",
 ]);
 
 const vars = mk.newCSSVariableMap({
-  favicon: [
-    await mk.loadSvgAsCssUrl('./favicon1.svg'),
-    await mk.loadSvgAsCssUrl('./favicon2.svg'),
-  ],
   header: [
     await mk.loadSvgAsCssUrl('./header1.svg'),
     await mk.loadSvgAsCssUrl('./header2.svg'),
@@ -21,10 +17,7 @@ const vars = mk.newCSSVariableMap({
 });
 
 mk.newHTML('ROOT_HTML', `
-<div class="${clss.ROOT_CLASS}">
-  <h3></h3>
-  <h2></h2>
-</div>
+  <h2 class="${clss.ROOT_CLASS}"></h2>
 `);
 
 mk.newHTML('CSS', `
@@ -38,40 +31,7 @@ ${DARKMODE_SELECTOR_VALUE}
   ${vars.toString(1)};
 }
 
-.${clss.ROOT_CLASS} h2,
-.${clss.ROOT_CLASS} h3
-{
-  margin: 0px;
-  padding: 0px;
-  font-size: 16px;
-  font-weight: 400;
-}
-
-.${clss.ROOT_CLASS} *
-{
-  box-sizing: border-box;
-}
-
 .${clss.ROOT_CLASS}
-{
-  display: flex;
-  height: 33px;
-}
-
-.${clss.ROOT_CLASS} > h3
-{
-  height: 100%;
-  width: 35px;
-  margin-right: 7px;
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position-y: center;
-  background-position-x: left;
-  background-image: ${vars.favicon.asVar()};
-  flex-shrink: 0;
-}
-
-.${clss.ROOT_CLASS} > h2
 {
   width: 77px;
   height: 100%;
@@ -82,22 +42,19 @@ ${DARKMODE_SELECTOR_VALUE}
   background-position-x: left;
   background-image: ${vars.header.asVar()};
   flex-shrink: 0;
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+  font-size: 16px;
+  font-weight: 400;
 }
 
 @media (device-width < ${HEADER_MOBILE_DEVICE_WIDTH})
 {
   .${clss.ROOT_CLASS}
   {
-    height: 130px;
-  }
-  .${clss.ROOT_CLASS} > h2
-  {
     width: 170px;
     background-size: 425px;
-  }
-  .${clss.ROOT_CLASS} > h3
-  {
-    display: none;
   }
 }
 `);
