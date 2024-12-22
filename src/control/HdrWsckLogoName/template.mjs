a9abc02c0df16f37609686bc1d99548c81250953
nxSerhey <yakubin.serhey@gmail.com>
2024-12-23 01:24:05 +0200

    WS Logo separation

---
+++ src/control/HdrWsckLogoName/template.mjs
@@ -0,0 +1,64 @@
+import ControlMaker from '../../lib/ControlMaker.mjs';
+
+import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
+import { HEADER_MOBILE_DEVICE_WIDTH } from '../../lib/WickedTheme.mjs';
+
+const mk = new ControlMaker('HdrWsckLogo', import.meta.url);
+
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",
+]);
+
+const vars = mk.newCSSVariableMap({
+  header: [
+    await mk.loadSvgAsCssUrl('./header1.svg'),
+    await mk.loadSvgAsCssUrl('./header2.svg'),
+  ],
+});
+
+mk.newHTML('ROOT_HTML', `
+  <h2 class="${clss.ROOT_CLASS}"></h2>
+`);
+
+mk.newHTML('CSS', `
+:root
+{
+  ${vars.toString(0)};
+}
+
+${DARKMODE_SELECTOR_VALUE}
+{
+  ${vars.toString(1)};
+}
+
+.${clss.ROOT_CLASS}
+{
+  height: 100%;
+  width: 145px;
+  margin-right: 15px;
+  background-size: 180px;
+  background-position-y: center;
+  background-position-x: left;
+  background-repeat: no-repeat;
+  background-image: ${vars.header.asVar()};
+  flex-shrink: 0;
+  margin: 0px;
+  padding: 0px;
+  font-size: 1em;
+  font-weight: 400;
+}
+
+@media (device-width < ${HEADER_MOBILE_DEVICE_WIDTH})
+{
+  .${clss.ROOT_CLASS}
+  {
+    width: 330px;
+    background-size: 410px;
+  }
+}
+`);
+
+export function buildComponent()
+{
+  return mk.buildComponent();
+}
