ea09bc96b723a399fe5dc8d063340c2a10046df8
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-01-13 00:06:12 +0200

    DBC link and logoName (#156)

---
+++ src/control/DBCLink/index.mjs
@@ -0,0 +1,4 @@
+import { BaseControl } from 'webnetq-js';
+
+export default class DBCTreeControl extends BaseControl {
+};
---
+++ src/control/DBCLink/template.mjs
@@ -0,0 +1,77 @@
+import ControlMaker from '../../lib/ControlMaker.mjs';
+import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
+import { HEADER_MOBILE_DEVICE_WIDTH } from '../../lib/WickedTheme.mjs';
+const mk = new ControlMaker('DBCLink', import.meta.url);
+
+const vars = mk.newCSSVariableMap({
+  OpS: ['#85858526'],
+  dbc_col: ['#5063b1'],
+});
+
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",,
+]);
+
+mk.newHTML('ROOT_HTML', `
+  <a href="https://github.com/ykbin/dbc" class="${clss.ROOT_CLASS} notranslate" translate="no" target="_blank" draggable="false">
+    Sources code <span>(GitHub)</span>
+  </a>
+`);
+
+mk.newCSS('CSS', `
+:root
+{
+  ${vars.toString(0)};
+}
+
+${DARKMODE_SELECTOR_VALUE}
+{
+  ${vars.toString(1)};
+}
+
+.${clss.ROOT_CLASS}
+{
+  display: block;
+  color: ${vars.dbc_col.asVar()};
+  width: auto;
+  height: 25px;
+  margin: 0 0 0 5px;
+  padding: 3px;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  font-family: Open Sans, Arial, sans-serif;
+  text-decoration: none;
+  flex-shrink: 0;
+}
+
+.${clss.ROOT_CLASS}:hover
+{
+  background-color: ${vars.OpS.asVar()};
+}
+
+@media (device-width < ${HEADER_MOBILE_DEVICE_WIDTH})
+{
+  .${clss.ROOT_CLASS}
+  {
+    font-size: 25px;
+  }
+  .${clss.ROOT_CLASS} span
+  {
+    display: none;
+  }
+}
+
+@media (device-width < 230px)
+{
+  .${clss.ROOT_CLASS}
+  {
+    display: none;
+  }
+}
+`);
+
+export function buildComponent()
+{
+  return mk.buildComponent();
+}
